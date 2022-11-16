8511286a7a6153a69d424969468f01814354bd98
NicholasMarkou <nmarkou2000@gmail.com>
2022-11-16 13:11:06 +0100

    Pages dynamically load

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,14 +4,41 @@ import { Link } from "react-router-dom";
 
 class Homepage extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('/pageTypes')
+            .then(res => res.json())
+            .then( links => {
+                this.setState({
+                    data: links,
+                    loaded: true
+                })
+            })
+    }
+
     render() {
         return (
             <div className="homepage-parent">
-                <Link className="column-div" to="/things-to-do">Things to do</Link>
-                <Link className="column-div" to="/language-prep">Language Prep</Link>
-                <Link className="column-div" to="/using-services">Utilizing Services in the Czech Republic</Link>
-                <Link className="column-div" to="/upcoming-trips">Upcoming trips offered by CIEE Prague</Link>
+                <>
+                    {
+                        this.state.loaded ? this.state.data.map(post => <Link className="column-div" to={"/view-posts/"+post.postType}>{post.title}</Link>) :
+                            <h1 style={{position: "absolute", left:0, right:0, marginLeft: "auto", marginRight: "auto", textAlign: "center"}}>Loading</h1>
+                    }
+                </>
+                {/*
+                <Link className="column-div" to="/view-posts/things-to-do">Things to do</Link>
+                <Link className="column-div" to="/view-posts/language-prep">Language Prep</Link>
+                <Link className="column-div" to="/view-posts/using-services">Utilizing Services in the Czech Republic</Link>
+                <Link className="column-div" to="/view-posts/upcoming-trips">Upcoming trips offered by CIEE Prague</Link>
                 <Link className="column-div" to="/admin-panel">Admin Panel (Temporary for Development Purposes)</Link>
+                */}
             </div>
         );
     }
--- src/pages/ViewPosts.js
+++ src/pages/ViewPosts.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PostBlock from "../components/PostBlock";
 import './styles/ThingToDo.css';
+import {
+    useLocation,
+    useNavigate,
+    useParams,
+} from "react-router-dom";
 
-class ThingsToDo extends React.Component {
+class ViewPosts extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             data: [],
             loaded: false
@@ -19,7 +24,7 @@ class ThingsToDo extends React.Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "postType": 'things-to-do'
+                "postType": this.props.router.params.postType
             })
         }).then(res => res.json())
             .then( posts => {
@@ -33,7 +38,7 @@ class ThingsToDo extends React.Component {
     render() {
         return(
             <div>
-                <h2>Things to do in Prague</h2>
+                <h2>View Posts</h2>
                 <div className="things-to-do-parent">
                     <>
                         {
@@ -47,5 +52,21 @@ class ThingsToDo extends React.Component {
     }
 }
 
+//https://reactrouter.com/en/v6.3.0/faq
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
 
-export default ThingsToDo;
+    return ComponentWithRouterProp;
+}
+
+export default withRouter(ViewPosts);
