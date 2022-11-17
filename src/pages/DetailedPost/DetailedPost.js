40e7755e503e1de5849ae6f2d4b2b288f31b0dd1
NicholasMarkou <nmarkou2000@gmail.com>
2022-11-17 12:49:52 +0100

    Image uploaded added

--- src/pages/DetailedPost/DetailedPost.js
+++ src/pages/DetailedPost/DetailedPost.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import "./DetailedPost.css";
-import exampleImage from "../../assets/Site T-Shirt_Prague.png";
+//import exampleImage from "../../assets/Site T-Shirt_Prague.png";
 import {useParams} from "react-router-dom";
 
 export function DetailedPost(props) {
@@ -19,13 +19,13 @@ export function DetailedPost(props) {
             })
         }).then(data => data.json())
             .then(d => setPostData(d))
-    })
+    }, [])
 
     return (
         <div className="">
             <h1>{postData.title}</h1>
             <h3>{postData.shortDescription}</h3>
-            <img src={exampleImage} alt="Example" width="25%"/>
+            <img className="detailed-image" src={"/getImage?_id="+id} alt="Example" width="25%"/>
             <p>{postData.description}</p>
         </div>
     )
