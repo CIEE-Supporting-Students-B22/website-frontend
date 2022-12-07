2acffe002a7b474652a97b35381dea85333ef3e3
NicholasMarkou <nmarkou2000@gmail.com>
2022-12-07 23:29:05 +0100

    detailed image editing, html in post support

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import logo from './assets/CIEE_Logo.jpg';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage.js'
 import React from 'react';
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 
-import { AdminPanel, ViewPosts, DetailedPost } from "./pages";
+import { AdminPanel, ViewPosts, DetailedPost, Login } from "./pages";
 import { NewPost, EditPost } from "./components";
 
 function App() {
 
-  return (
-      <BrowserRouter>
-          <div className="App">
-              <Link to="/"><img src={logo} alt={"Logo"} className="ciee-logo"/></Link>
-              <Routes>
-                  <Route path="/" element={<Homepage/>} />
-                  <Route path="view-posts/:postType" element={<ViewPosts />}/>
-                  <Route path="/admin-panel" element={<AdminPanel />} />
-                  <Route path="/newPost/:postType" element={<NewPost />}/>
-                  <Route path="/editPost/:id" element={<EditPost />}/>
-                  <Route path="/post/:id" element={<DetailedPost />}/>
-              </Routes>
-          </div>
-      </BrowserRouter>
-  );
+      return (
+              <div className="App">
+                  <Link to="/"><img src={logo} alt={"Logo"} className="ciee-logo"/></Link>
+                  <Routes>
+                      <Route path="/" element={<Homepage/>} />
+                      <Route path="/login" element={<Login/>} />
+                      <Route path="view-posts/:postType" element={<ViewPosts />}/>
+                      <Route path="/admin-panel" element={<AdminPanel />} />
+                      <Route path="/newPost/:postType" element={<NewPost />}/>
+                      <Route path="/editPost/:id" element={<EditPost />}/>
+                      <Route path="/post/:id" element={<DetailedPost />}/>
+                  </Routes>
+              </div>
+      );
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as AdminPanel } from "./AdminPanel/AdminPanel";
 export { DetailedPost } from "./DetailedPost/DetailedPost";
 export { default as Homepage } from "./Homepage/Homepage";
 export { default as ViewPosts } from "./ViewPosts/ViewPosts";
+export { Login } from "./Login/Login";
